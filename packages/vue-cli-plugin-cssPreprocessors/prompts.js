30e285ec7d526f499e846c81d522d4c426f89e57
huang.peng <huang.peng@immomo.com>
2021-12-31 15:52:32 +0800

    fix: 🐛 测试改变css

---
+++ packages/vue-cli-plugin-cssPreprocessors/prompts.js
@@ -0,0 +1,25 @@
+module.exports = pkg => {
+  const prompts = [{
+    name: 'cssPreprocessor',
+    when: answers => answers.features.includes('css-preprocessor'),
+    type: 'list',
+    message: `Pick a CSS pre-processor${process.env.VUE_CLI_API_MODE ? '' : ` (${notice})`}:`,
+    description: `${notice}.`,
+    choices: [{
+        name: 'Sass/SCSS (with dart-sass)',
+        value: 'dart-sass'
+      },
+      {
+        name: 'Less',
+        value: 'less'
+      },
+      {
+        name: 'Stylus',
+        value: 'stylus'
+      }
+    ]
+  }]
+
+
+  return prompts
+}
