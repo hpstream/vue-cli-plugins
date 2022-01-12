74480f941fe80f49707add97a8405a5174ef509c
huang.peng <huang.peng@immomo.com>
2022-01-12 19:32:17 +0800

    fix: 🐛 修复模版问题

--- packages/vue-cli-plugin-build/index.js
+++ packages/vue-cli-plugin-build/index.js
@@ -1,3 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+function getEntryPages() {
+  const result = {};
+  try {
+    const pagesPath = path.resolve(process.cwd(), './src/pages');
+    if (fs.existsSync(pagesPath)) {
+      fs.readdirSync(pagesPath)
+        .filter((item) => item.indexOf('.') !== 0)
+        .forEach((item) => {
+          result[item] = {
+            // page 的入口
+            entry: `src/pages/${item}/index.ts`,
+            // 模板来源
+            template: `src/pages/${item}/index.html`,
+            // 在 dist/index.html 的输出
+            filename: `${item}.html`,
+          };
+        });
+    }
+  } catch (e) {
+    console.log(e.message); // eslint-disable-line
+  }
+  return result;
+}
 module.exports = (api, vueOptions) => {
- console.log(vueOptions)
+
+  api.registerCommand(
+    'greet', {
+      description: 'Write a greeting to the console',
+      usage: 'vue-cli-service greet'
+    },
+    () => {
+      console.log(`👋  Hello`)
+    }
+  )
+  const pages = getEntryPages();
+  vueOptions.pages = pages;
+
 }
